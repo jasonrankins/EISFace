e30c3e4e7ada685c19beab5ffacd8ca16486be77
Jason Rankins <jason@thumblabs.com>
2013-03-06 00:08:31 -0500

    Porting FaceOSC

--- EISFace/src/testApp.h
+++ EISFace/src/testApp.h
@@ -7,29 +7,63 @@
 #include "ofxCv.h"
 #include "ofxFaceTracker.h"
 
+#include "ofxOsc.h"
+#include "ofxXmlSettings.h"
+
 class testApp : public ofxiPhoneApp{
-	
-    public:
-        void setup();
-        void update();
-        void draw();
-        void exit();
-	
-        void touchDown(ofTouchEventArgs & touch);
-        void touchMoved(ofTouchEventArgs & touch);
-        void touchUp(ofTouchEventArgs & touch);
-        void touchDoubleTap(ofTouchEventArgs & touch);
-        void touchCancelled(ofTouchEventArgs & touch);
+public:
+    
+#pragma mark - Function Declarations
+    void loadSettings();
+    
+    void clearBundle();
+    template <class T>
+    void addMessage(string address, T data);
+    void sendBundle();
+    
+    void setup();
+    void update();
+    void draw();
+    void keyPressed(int key);
+    
+    void setVideoSource(bool useCamera);
+    
+    void exit();
+    
+    void touchDown(ofTouchEventArgs & touch);
+    void touchMoved(ofTouchEventArgs & touch);
+    void touchUp(ofTouchEventArgs & touch);
+    void touchDoubleTap(ofTouchEventArgs & touch);
+    void touchCancelled(ofTouchEventArgs & touch);
+    
+    void lostFocus();
+    void gotFocus();
+    void gotMemoryWarning();
+    void deviceOrientationChanged(int newOrientation);
+    
+#pragma mark - Member Variables
+    bool bUseCamera, bPaused;
+    
+    int camWidth, camHeight;
+    int movieWidth, movieHeight;
+    int sourceWidth, sourceHeight;
+    
+    string host;
+    int port;
+    ofxOscSender osc;
+    ofxOscBundle bundle;
+    
+    ofVideoGrabber cam;
+    ofVideoPlayer movie;
+    ofBaseVideoDraws *videoSource;
+    
+    ofxFaceTracker tracker;
+    float scale;
+    ofMatrix4x4 rotationMatrix;
+    
+    bool bDrawMesh;
 
-        void lostFocus();
-        void gotFocus();
-        void gotMemoryWarning();
-        void deviceOrientationChanged(int newOrientation);
-
-        ofVideoGrabber cam;
-        ofxFaceTracker tracker;
-        ofImage image;
-        int framePadding;
+    ofImage image;
 };
 
 
--- EISFace/src/testApp.mm
+++ EISFace/src/testApp.mm
@@ -1,110 +1,302 @@
 #include "testApp.h"
 
 using namespace ofxCv;
+using namespace cv;
 
-//--------------------------------------------------------------
-void testApp::setup(){	
-	// initialize the accelerometer
-	ofxAccelerometer.setup();
-	
-	//If you want a landscape oreintation 
-	//iPhoneSetOrientation(OFXIPHONE_ORIENTATION_LANDSCAPE_RIGHT);
-	
-	ofBackground(127,127,127);
-    
-    ofSetFrameRate(30);
-    ofSetVerticalSync(false);
-	ofSetDrawBitmapMode(OF_BITMAPMODE_MODEL_BILLBOARD);
-	
-    cam.setDeviceID(1);
-    cam.initGrabber(cam.getWidth(), cam.getHeight());
-    
-	tracker.setup();
-    tracker.setRescale(0.25);
-    framePadding = 0;
+#pragma mark - OSC Methods
+template <>
+void testApp::addMessage(string address, ofVec3f data) {
+	ofxOscMessage msg;
+	msg.setAddress(address);
+	msg.addFloatArg(data.x);
+	msg.addFloatArg(data.y);
+	msg.addFloatArg(data.z);
+	bundle.addMessage(msg);
 }
 
-//--------------------------------------------------------------
-void testApp::update(){
-    cam.update();
-    if(cam.isFrameNew()){// && cam.getPixelsRef().size() > 0) {
-        image.setFromPixels(cam.getPixels(), cam.getWidth(), cam.getHeight(), OF_IMAGE_COLOR);
-        tracker.update(toCv(image));
-        //Mat pixels = toCv(cam.getPixelsRef());
-		//tracker.update(toCv(cam.getPixelsRef()));
+template <>
+void testApp::addMessage(string address, ofVec2f data) {
+	ofxOscMessage msg;
+	msg.setAddress(address);
+	msg.addFloatArg(data.x);
+	msg.addFloatArg(data.y);
+	bundle.addMessage(msg);
+}
+
+template <>
+void testApp::addMessage(string address, float data) {
+	ofxOscMessage msg;
+	msg.setAddress(address);
+	msg.addFloatArg(data);
+	bundle.addMessage(msg);
+}
+
+template <>
+void testApp::addMessage(string address, int data) {
+	ofxOscMessage msg;
+	msg.setAddress(address);
+	msg.addIntArg(data);
+	bundle.addMessage(msg);
+}
+
+void testApp::sendBundle() {
+	osc.sendBundle(bundle);
+}
+
+void testApp::clearBundle() {
+	bundle.clear();
+}
+
+#pragma mark - Application Lifecycle
+void testApp::setup() {
+	ofSetVerticalSync(true);
+#ifdef TARGET_OSX
+	ofSetDataPathRoot("../Resources/data/");
+#endif
+	loadSettings();
+}
+
+void testApp::update() {
+	if(bPaused)
+		return;
+    
+	videoSource->update();
+	if(videoSource->isFrameNew()) {
+		tracker.update(toCv(*videoSource));
+        
+		clearBundle();
+        
+		if(tracker.getFound()) {
+			addMessage("/found", 1);
+            
+			ofVec2f position = tracker.getPosition();
+			addMessage("/pose/position", position);
+			scale = tracker.getScale();
+			addMessage("/pose/scale", scale);
+			ofVec3f orientation = tracker.getOrientation();
+			addMessage("/pose/orientation", orientation);
+            
+			addMessage("/gesture/mouth/width", tracker.getGesture(ofxFaceTracker::MOUTH_WIDTH));
+			addMessage("/gesture/mouth/height", tracker.getGesture(ofxFaceTracker::MOUTH_HEIGHT));
+			addMessage("/gesture/eyebrow/left", tracker.getGesture(ofxFaceTracker::LEFT_EYEBROW_HEIGHT));
+			addMessage("/gesture/eyebrow/right", tracker.getGesture(ofxFaceTracker::RIGHT_EYEBROW_HEIGHT));
+			addMessage("/gesture/eye/left", tracker.getGesture(ofxFaceTracker::LEFT_EYE_OPENNESS));
+			addMessage("/gesture/eye/right", tracker.getGesture(ofxFaceTracker::RIGHT_EYE_OPENNESS));
+			addMessage("/gesture/jaw", tracker.getGesture(ofxFaceTracker::JAW_OPENNESS));
+			addMessage("/gesture/nostrils", tracker.getGesture(ofxFaceTracker::NOSTRIL_FLARE));
+		} else {
+			addMessage("/found", 0);
+		}
+        
+		sendBundle();
+        
+		rotationMatrix = tracker.getRotationMatrix();
 	}
 }
 
-//--------------------------------------------------------------
-void testApp::draw(){
+void testApp::draw() {
 	ofSetColor(255);
-    cam.draw(0, 0, 768, 1024);
-	ofDrawBitmapString(ofToString((int) ofGetFrameRate()), 10, 20);
-	
-	ofPolyline leftEye = tracker.getImageFeature(ofxFaceTracker::LEFT_EYE);
-	ofPolyline rightEye = tracker.getImageFeature(ofxFaceTracker::RIGHT_EYE);
-	ofPolyline faceOutline = tracker.getImageFeature(ofxFaceTracker::FACE_OUTLINE);
-	
-	ofSetLineWidth(2);
-	ofSetColor(ofColor::red);
-	leftEye.draw();
-	ofSetColor(ofColor::green);
-	rightEye.draw();
-	ofSetColor(ofColor::blue);
-	faceOutline.draw();
-	
-	ofSetLineWidth(1);
-	ofSetColor(255);
-	tracker.draw();
+	videoSource->draw(0, 0);
+    
+	if(tracker.getFound()) {
+		ofDrawBitmapString(ofToString((int) ofGetFrameRate()), 10, 20);
+        
+		if(bDrawMesh) {
+			ofSetLineWidth(1);
+			//tracker.draw();
+			tracker.getImageMesh().drawWireframe();
+            
+			ofPushView();
+			ofSetupScreenOrtho(sourceWidth, sourceHeight, OF_ORIENTATION_UNKNOWN, true, -1000, 1000);
+			ofVec2f pos = tracker.getPosition();
+			ofTranslate(pos.x, pos.y);
+			applyMatrix(rotationMatrix);
+			ofScale(10,10,10);
+			ofDrawAxis(scale);
+			ofPopView();
+		}
+	} else {
+		ofDrawBitmapString("searching for face...", 10, 20);
+	}
+    
+	if(bPaused) {
+		ofSetColor(255, 0, 0);
+		ofDrawBitmapString( "paused", 10, 32);
+	}
+    
+	if(!bUseCamera) {
+		ofSetColor(255, 0, 0);
+		ofDrawBitmapString("speed "+ofToString(movie.getSpeed()), ofGetWidth()-100, 20);
+	}
 }
 
-//--------------------------------------------------------------
 void testApp::exit(){
 
 }
 
-//--------------------------------------------------------------
+
+void testApp::keyPressed(int key) {
+	switch(key) {
+		case 'r':
+			tracker.reset();
+			break;
+		case 'm':
+			bDrawMesh = !bDrawMesh;
+			break;
+		case 'p':
+			bPaused = !bPaused;
+			break;
+		case OF_KEY_UP:
+			movie.setSpeed(ofClamp(movie.getSpeed()+0.2, -16, 16));
+			break;
+		case OF_KEY_DOWN:
+			movie.setSpeed(ofClamp(movie.getSpeed()-0.2, -16, 16));
+			break;
+	}
+}
+
+#pragma mark - Settings
+void testApp::loadSettings() {
+	ofxXmlSettings xml;
+	xml.loadFile("settings.xml");
+    
+	bool bUseCamera = true;
+    
+	xml.pushTag("source");
+	if(xml.getNumTags("useCamera") > 0) {
+		bUseCamera = xml.getValue("useCamera", 0);
+	}
+	xml.popTag();
+    
+	xml.pushTag("camera");
+	if(xml.getNumTags("device") > 0) {
+		cam.setDeviceID(xml.getValue("device", 0));
+	}
+	if(xml.getNumTags("framerate") > 0) {
+		cam.setDesiredFrameRate(xml.getValue("framerate", 30));
+	}
+	camWidth = xml.getValue("width", 640);
+	camHeight = xml.getValue("height", 480);
+	cam.initGrabber(camWidth, camHeight);
+	xml.popTag();
+    
+	xml.pushTag("movie");
+	if(xml.getNumTags("filename") > 0) {
+		string filename = ofToDataPath((string) xml.getValue("filename", ""));
+		if(!movie.loadMovie(filename)) {
+			ofLog(OF_LOG_ERROR, "Could not load movie \"%s\", reverting to camera input", filename.c_str());
+			bUseCamera = true;
+		}
+		movie.play();
+	}
+	else {
+		ofLog(OF_LOG_ERROR, "Movie filename tag not set in settings, reverting to camera input");
+		bUseCamera = true;
+	}
+	if(xml.getNumTags("volume") > 0) {
+		float movieVolume = ofClamp(xml.getValue("volume", 1.0), 0, 1.0);
+		movie.setVolume(movieVolume);
+	}
+	if(xml.getNumTags("speed") > 0) {
+		float movieSpeed = ofClamp(xml.getValue("speed", 1.0), -16, 16);
+		movie.setSpeed(movieSpeed);
+	}
+	bPaused = false;
+	movieWidth = movie.getWidth();
+	movieHeight = movie.getHeight();
+	xml.popTag();
+    
+	if(bUseCamera) {
+		ofSetWindowShape(camWidth, camHeight);
+		setVideoSource(true);
+	}
+	else {
+		ofSetWindowShape(movieWidth, movieHeight);
+		setVideoSource(false);
+	}
+    
+	xml.pushTag("face");
+	if(xml.getNumTags("rescale")) {
+		tracker.setRescale(xml.getValue("rescale", 1.));
+	}
+	if(xml.getNumTags("iterations")) {
+		tracker.setIterations(xml.getValue("iterations", 5));
+	}
+	if(xml.getNumTags("clamp")) {
+		tracker.setClamp(xml.getValue("clamp", 3.));
+	}
+	if(xml.getNumTags("tolerance")) {
+		tracker.setTolerance(xml.getValue("tolerance", .01));
+	}
+	if(xml.getNumTags("attempts")) {
+		tracker.setAttempts(xml.getValue("attempts", 1));
+	}
+	bDrawMesh = true;
+	if(xml.getNumTags("drawMesh")) {
+		bDrawMesh = (bool) xml.getValue("drawMesh", 1);
+	}
+	tracker.setup();
+	xml.popTag();
+    
+	xml.pushTag("osc");
+	host = xml.getValue("host", "localhost");
+	port = xml.getValue("port", 8338);
+	osc.setup(host, port);
+	xml.popTag();
+    
+	osc.setup(host, port);
+}
+
+void testApp::setVideoSource(bool useCamera) {
+    
+	bUseCamera = useCamera;
+    
+	if(bUseCamera) {
+		videoSource = &cam;
+		sourceWidth = camWidth;
+		sourceHeight = camHeight;
+	}
+	else {
+		videoSource = &movie;
+		sourceWidth = movieWidth;
+		sourceHeight = movieHeight;
+	}
+}
+
+#pragma mark - Touch Events
 void testApp::touchDown(ofTouchEventArgs & touch){
     tracker.reset();
 }
 
-//--------------------------------------------------------------
 void testApp::touchMoved(ofTouchEventArgs & touch){
-
+    
 }
 
-//--------------------------------------------------------------
 void testApp::touchUp(ofTouchEventArgs & touch){
-
+    
 }
 
-//--------------------------------------------------------------
 void testApp::touchDoubleTap(ofTouchEventArgs & touch){
-
+    
 }
 
-//--------------------------------------------------------------
 void testApp::touchCancelled(ofTouchEventArgs & touch){
     
 }
 
-//--------------------------------------------------------------
+#pragma mark - Application Delegate
 void testApp::lostFocus(){
-
+    
 }
 
-//--------------------------------------------------------------
 void testApp::gotFocus(){
-
+    
 }
 
-//--------------------------------------------------------------
 void testApp::gotMemoryWarning(){
-
+    
 }
 
-//--------------------------------------------------------------
 void testApp::deviceOrientationChanged(int newOrientation){
-
+    
 }
-
